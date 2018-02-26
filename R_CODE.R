c1ebbbf56d1771ffd2c076e656df61c068420509
Lekshmi Narasimman Madras Narayanan <32528025+lm344@users.noreply.github.com>
2018-02-26 01:00:06 -0500

    Add files via upload

---
+++ R_CODE.R
@@ -0,0 +1,13 @@
+install.packages("visreg")
+library(ggplot2)
+library(visreg)
+data <- read.csv("diabetes.csv",head=TRUE)
+str(data)
+head(data) 
+summary(data)
+pairs(data)
+train <- data[1:500,]
+test <- data[501:768,]
+trainmodel <- lm(Outcome ~ ., data = train)
+result <- predict(trainmodel,test)
+summary(result)
